948e4988eec24099f1f3bd7465777492c2bedfdf
Vadim Tkachenko <vt@homeclimatecontrol.com>
2015-06-15 13:05:51 -0700

    Marked unimplemented class as "hard fail"; @Ignore'd the test case

--- jukebox-service/src/test/java/net/sf/jukebox/aggregator/HomogenousAggregatorTest.java
+++ jukebox-service/src/test/java/net/sf/jukebox/aggregator/HomogenousAggregatorTest.java
@@ -3,46 +3,39 @@ package net.sf.jukebox.aggregator;
 import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import org.junit.Ignore;
+
 import junit.framework.TestCase;
 import net.sf.jukebox.aggregator.RunnableAggregator.Error;
-import org.apache.log4j.BasicConfigurator;
 
 /**
  *
  * @author Copyright &copy; <a href="mailto:vt@freehold.crocodile.org">Vadim Tkachenko</a> 2007-2009
  */
+@Ignore
 public class HomogenousAggregatorTest extends TestCase {
 
-    private boolean loggerInitialized = false;
-    
-    @Override
-    public void setUp() {
-        if (!loggerInitialized) {
-            BasicConfigurator.configure();
-            loggerInitialized = true;
-        }
-    }
-    
     public void testX2() {
-        
+
         HomogenousAggregator<Integer, Integer, IOException> aggregator = new HomogenousAggregator<Integer, Integer, IOException>();
         WorkerFactory<Integer, Integer, IOException> workerFactory = new X2WorkerFactory();
-        
+
         BlockingQueue<Integer> requestQueue = new LinkedBlockingQueue<Integer>();
         BlockingQueue<Integer> responseQueue = new LinkedBlockingQueue<Integer>();
-        
+
         aggregator.process(requestQueue, responseQueue, workerFactory);
     }
-    
+
     public class X2WorkerFactory implements WorkerFactory<Integer, Integer, IOException> {
 
         public Worker<Integer, Integer, Runnable, IOException> createWorker(Integer rq, BlockingQueue<Integer> responseQueue) {
             throw new UnsupportedOperationException("Not supported yet.");
         }
     }
-    
+
     public class X2Worker extends Worker<Integer, Integer, Runnable, IOException> {
-        
+
         public X2Worker(Integer rq) {
             super(rq);
         }
@@ -58,6 +51,6 @@ public class HomogenousAggregatorTest extends TestCase {
                 throw new IOException("Interrupted", ex);
             }
         }
-        
+
     }
 }
