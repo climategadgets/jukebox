26ec2852a026d8f167621513d1e35109384c322b
Vadim Tkachenko <vt@homeclimatecontrol.com>
2015-06-17 11:59:45 -0700

    Added a test case

--- jukebox-sem/src/test/java/net/sf/jukebox/sem/ACTTest.java
+++ jukebox-sem/src/test/java/net/sf/jukebox/sem/ACTTest.java
@@ -34,4 +34,51 @@ public class ACTTest extends TestCase {
         
         assertEquals("Wrong string representation", "ACT." + Integer.toHexString(act.hashCode()) + "(Hi there:complete)", act.toString());
     }
+
+    public void testLifecycle2() throws InterruptedException, SemaphoreTimeoutException {
+        
+        final ACT act = new ACT();
+        final String message = "Done!";
+        
+        assertFalse(act.isComplete());
+        
+        assertEquals("Wrong string representation", "ACT." + Integer.toHexString(act.hashCode()) + "(null:waiting)", act.toString());
+        
+        Runnable r = new Runnable() {
+
+            @Override
+            public void run() {
+                
+                try {
+                    
+                    Thread.sleep(100);
+                    act.complete(true, message);
+                    
+                } catch (InterruptedException ex) {
+                    
+                    // The trace is irrelevant here
+                    fail("Unexpected exception: " + ex.toString());
+                }
+            }
+        };
+        
+        new Thread(r).start();
+
+        assertTrue(act.waitFor(150));
+        assertTrue(act.isComplete());
+        assertTrue(act.waitFor(150));
+
+        assertEquals("Wrong user object", message, act.getUserObject());
+        assertTrue("Wrong status", act.getStatus());
+        
+        assertEquals("Wrong string representation", "ACT." + Integer.toHexString(act.hashCode()) + "(Done!:complete)", act.toString());
+
+        try {
+            act.trigger(false);
+            fail("Should've failed by now");
+
+        } catch (IllegalStateException ex) {
+            assertEquals("Wrong exception message", "Can't trigger ACT more than once", ex.getMessage());
+        }
+    }
 }
