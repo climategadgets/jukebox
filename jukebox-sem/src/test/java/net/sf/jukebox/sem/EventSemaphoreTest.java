09c434501127c3e196e45df3e449844026a1a859
Vadim Tkachenko <vt@homeclimatecontrol.com>
2015-06-18 23:07:29 -0700

    Added test cases

--- jukebox-sem/src/test/java/net/sf/jukebox/sem/EventSemaphoreTest.java
+++ jukebox-sem/src/test/java/net/sf/jukebox/sem/EventSemaphoreTest.java
@@ -1,20 +1,47 @@
 package net.sf.jukebox.sem;
 
+import java.util.Random;
+
 import net.sf.jukebox.util.PackageNameStripper;
 import junit.framework.TestCase;
 
 public class EventSemaphoreTest extends TestCase {
+    
+    private final Random rg = new Random();
 
     public void testName() {
         
-        String name = Integer.toHexString(hashCode());
-        Semaphore semNamed = new EventSemaphore(name);
-        Semaphore semOwned = new EventSemaphore(this);
-        Semaphore semOwnedNamed = new EventSemaphore(this, name);
+        String name = Integer.toHexString(rg.nextInt());
         
+        EventSemaphore sem = new EventSemaphore();
+        EventSemaphore semNamed = new EventSemaphore(name);
+        EventSemaphore semOwned = new EventSemaphore(this);
+        EventSemaphore semOwnedNamed = new EventSemaphore(this, name);
+        
+        assertEquals("Wrong name", null, sem.getName());
         assertEquals("Wrong name", name, semNamed.getName());
         assertEquals("Wrong name", PackageNameStripper.stripPackage(getClass().getName()) + "/" + Integer.toHexString(hashCode()) + "/", semOwned.getName());
         assertEquals("Wrong name", PackageNameStripper.stripPackage(getClass().getName()) + "/" + Integer.toHexString(hashCode()) + "/" + name, semOwnedNamed.getName());
+        
+        assertEquals("Wrong string representation", toString(sem), sem.toString());
+        assertEquals("Wrong string representation", toString(semNamed), semNamed.toString());
+        assertEquals("Wrong string representation", toString(semOwned), semOwned.toString());
+        assertEquals("Wrong string representation", toString(semOwnedNamed), semOwnedNamed.toString());
+    }
+    
+    private String toString(EventSemaphore target) {
+        
+        StringBuilder sb = new StringBuilder("(EventSem");
+        String name = target.getName();
+
+        if (!"".equals(name)) {
+
+            sb.append("[").append(name).append("]");
+        }
+
+        sb.append(".").append(Integer.toHexString(target.hashCode())).append(":").append(target.getStatus()).append(")");
+
+        return sb.toString();
     }
     
     public void testPost() {
